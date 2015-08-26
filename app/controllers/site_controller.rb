3522cd0ce9bd68b3d92f8113ac46f8d3898ebb70
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-08-26 20:19:47 -0300

    Add cloudinary configuration for restaurant photos. Throw possible information in main partial.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,7 @@
 class SiteController < ApplicationController
+
   def index
+    @restaurant = Redo::Restaurant.find_by(slug: params[:slug])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get "/" => "site#index"
+  get "/:slug" => "site#index"
 end
