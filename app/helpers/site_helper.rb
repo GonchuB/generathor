b70a3d1fcbfbcd33b0d6f3de504256cf080933af
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-08-26 22:13:18 -0300

    Add google map api.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,9 @@
 module SiteHelper
+
+  def google_map_url(restaurant)
+    lat, long = restaurant.location.split(",")
+
+    "https://www.google.com/maps/embed/v1/place?q=#{lat}%2C#{long}&key=#{Rails.application.secrets.maps_api_key}"
+  end
+
 end
